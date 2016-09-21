70b5483753008b61ac8a5c7c571cd4b0080c9152
Vitali Ihnatsenka <vitt.ignat@gmail.com>
2016-09-21 21:07:38 +0300

    jdbc

--- src/main/java/by/epam/jmp10/Main.java
+++ src/main/java/by/epam/jmp10/Main.java
@@ -2,6 +2,8 @@ package by.epam.jmp10;
 
 
 import java.sql.*;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Vitali on 20.09.2016.
@@ -11,51 +13,70 @@ public class Main {
 
     static final String USER = "root";
     static final String PASS = "";
+    static final int MAX_TABLES = 25;
+    static final int MAX_THREADS = 25;
+    static final int MAX_COLUMNS = 10;
+    static final int MAX_ROWS = 100;
 
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        AtomicInteger tablesNumber = new AtomicInteger(new Random().nextInt(MAX_TABLES) + 1);
+        int threadsNumber = new Random().nextInt(MAX_THREADS) + 1;
 
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-            String sql;
-            sql = "SELECT id, first, last, age FROM Employees";
-            ResultSet rs = stmt.executeQuery(sql);
+        for (int i = 0; i < threadsNumber; i++) {
+            new Thread(() -> {
+                Connection conn = null;
+                Statement stmt = null;
+                try{
+                    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    conn.setAutoCommit(false);
+                    stmt = conn.createStatement();
+                    int tableNumber = tablesNumber.getAndDecrement();
+                    while(tableNumber > 0){
+                        String tableName = "Table_" + tableNumber;
+                        int numberColumns = new Random().nextInt(MAX_COLUMNS) + 1;
+                        StringBuilder sql = new StringBuilder("CREATE TABLE " + tableName + "(");
+                        for (int j = 0; j < numberColumns; j++) {
+                            sql.append("Column_" + j + " varchar(255)");
+                            if(j != numberColumns-1){
+                                sql.append(",");
+                            }
+                        }
+                        sql.append(");");
+                        stmt.execute(sql.toString());
 
-            while(rs.next()){
-                int id  = rs.getInt("id");
-                int age = rs.getInt("age");
-                String first = rs.getString("first");
-                String last = rs.getString("last");
-
-                System.out.print("ID: " + id);
-                System.out.print(", Age: " + age);
-                System.out.print(", First: " + first);
-                System.out.println(", Last: " + last);
-            }
-            rs.close();
-            stmt.close();
-            conn.close();
-        }catch(SQLException se){
-            se.printStackTrace();
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            try{
-                if(stmt!=null)
-                    stmt.close();
-            }catch(SQLException se2){
-            }
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }
+                        int rowsNumber = new Random().nextInt(MAX_ROWS) + 1;
+                        for (int j = 0; j < rowsNumber; j++) {
+                            sql = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
+                            for (int k = 0; k < numberColumns; k++) {
+                                sql.append("'VALUE_" + k + "'");
+                                if(k != numberColumns-1){
+                                    sql.append(",");
+                                }
+                            }
+                            sql.append(");");
+                            stmt.execute(sql.toString());
+                        }
+                        tableNumber = tablesNumber.getAndDecrement();
+                    }
+                    conn.commit();
+                }catch(SQLException se){
+                    se.printStackTrace();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }finally{
+                    try{
+                        if(stmt!=null)
+                            stmt.close();
+                    }catch(SQLException se2){
+                    }
+                    try{
+                        if(conn!=null)
+                            conn.close();
+                    }catch(SQLException se){
+                        se.printStackTrace();
+                    }
+                }
+            }).start();
         }
-        System.out.println("Goodbye!");
     }
 }
